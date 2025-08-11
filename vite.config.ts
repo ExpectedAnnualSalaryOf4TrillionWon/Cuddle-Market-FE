a08cbc32ffcbc6eb9f0c6b81fb58c4f5cb578339
jjub0217 <noonssub1004@gmail.com>
2025-08-11 11:45:09 +0900

    docs: 경로 alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@assets', replacement: '/src/assets' },
+      { find: '@components', replacement: '/src/components' },
+      { find: '@icons', replacement: '/src/components/icons' },
+      { find: '@features', replacement: '/src/features' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@common', replacement: '/src/components/common' },
+    ],
+  },
 });
