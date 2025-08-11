0423feec366c056bddd6921f5f0e375ae2e95f3b
jjub0217 <noonssub1004@gmail.com>
2025-08-11 10:03:07 +0900

    Feat: 푸터 컴포넌트 생성 및 레이아웃, 디자인 추가

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,80 @@
+import logoImage from '../../../public/assets/images/CuddleMarketLogo.png';
+
+const Footer = () => {
+  return (
+    <footer className="mt-2xl tablet:mt-3xl border-t border-border bg-light">
+      <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-4 max-w-[var(--container-max-width)] mx-auto px-lg py-xl tablet:py-2xl gap-xl tablet:gap-2xl">
+        <div className="flex gap-sm">
+          <div className="flex justify-center w-6 h-6 rounded bg-primary">
+            <img src={logoImage} alt="Cuddle Market" className="w-auto h-12 object-contain" />
+          </div>
+          <h3 className="heading4 text-text-primary">펫마켓</h3>
+        </div>
+
+        <div>
+          <h4 className="mb-md tablet:mb-lg heading5 text-text-primary">고객센터</h4>
+          <ul className="space-y-xs tablet:space-y-sm bodySmall text-text-secondary">
+            <li>
+              <a href="#" className="hover:text-primary">
+                공지사항
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                자주묻는질문
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                1:1 문의
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                신고하기
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="mb-md tablet:mb-lg heading5 text-text-primary">이용안내</h4>
+          <ul className="space-y-xs tablet:space-y-sm bodySmall text-text-secondary">
+            <li>
+              <a href="#" className="hover:text-primary">
+                이용약관
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                개인정보처리방침
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                안전거래가이드
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-primary">
+                판매자가이드
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 className="mb-md tablet:mb-lg heading5 text-text-primary">연락처</h4>
+          <ul className="space-y-xs tablet:space-y-sm bodySmall text-text-secondary">
+            <li>고객센터: 1588-0000</li>
+            <li>평일 09:00 - 18:00</li>
+            <li>주말/공휴일 휴무</li>
+            <li>help@petmarket.co.kr</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
