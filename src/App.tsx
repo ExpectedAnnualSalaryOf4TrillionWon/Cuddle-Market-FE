041893f8dec9cc58a9e79e88e7fd4f84224e748f
dirage1 <dirage4635@gmail.com>
2025-08-11 15:52:58 +0900

    chore: 경로지정 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useLocation } from 'react-router-dom';
-import Footer from '@layout';
+import Footer from '@layout/Footer';
 import MainHeader from './components/layout/Header';
 import ChatButton from './features/chat/ChatButton';
 function App() {
