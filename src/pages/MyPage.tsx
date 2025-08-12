48594dc8b4755fbb91991dd7c6320a5ebe00ae01
dirage1 <dirage4635@gmail.com>
2025-08-12 17:43:55 +0900

    feat: 마이페이지 생성과정 확인을 위한 기본 div 생성, 라우트 설정

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,9 @@
+const MyPage = () => {
+  return (
+    <>
+      <div className="max-w-[var(--container-max-width)] mx-auto px-lg py-md bg-secondary "></div>
+    </>
+  );
+};
+
+export default MyPage;
