2447392eaaba4ec617f87bdf99c007ff4151f929
dirage1 <dirage4635@gmail.com>
2025-08-14 17:45:09 +0900

    fix:app.tsx에서 location 관련 안쓰는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import Footer from '@layout/Footer';
-import { useLocation } from 'react-router-dom';
 import ChatButton from './features/chat/ChatButton';
 import AppRoutes from './routes/index';
 
 function App() {
-  const location = useLocation();
   return (
     <>
       <AppRoutes />
