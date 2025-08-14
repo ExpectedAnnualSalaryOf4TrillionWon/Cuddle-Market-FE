ed8388c7a96b3b0dd3b09322c067596d55e56b4e
dirage1 <dirage4635@gmail.com>
2025-08-14 14:09:04 +0900

    design: 로그아웃 모달창 깨짐방지를 위한 너비설정 삭제

--- src/components/common/confirmModal.tsx
+++ src/components/common/confirmModal.tsx
@@ -7,7 +7,7 @@ const ConfirmModal = () => {
 
   return (
     <div className="fixed inset-0 bg-black/10 flex justify-center items-center z-[9999]">
-      <div className="bg-secondary p-2xl rounded-md w-[15vW] min-w-[150px] max-w-p[300px]">
+      <div className="bg-secondary p-2xl rounded-md">
         <p className="mb-4 text-heading5">{message}</p>
         <div className="flex justify-around">
           <button
