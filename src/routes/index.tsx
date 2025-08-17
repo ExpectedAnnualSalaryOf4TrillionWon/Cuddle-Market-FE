8339d26ffe6b7b1b45113c9d1edf35f0116f5721
jjub0217 <noonssub1004@gmail.com>
2025-08-17 10:11:12 +0900

    fix: vercel 에러 수정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import Home from '@pages/Home';
 
 import KakaoCallback from '@pages/KakaoCallback';
 import Login from '@pages/Login';
+import MyPage from '@pages/MyPage';
 import ProductDetail from '@pages/ProductDetail';
 import ProductPost from '@pages/ProductPost';
 import Signup from '@pages/Signup';
 import UserPage from '@pages/UserPage';
 import { Outlet, Route, Routes } from 'react-router-dom';
-import MyPage from '@pages/MyPage';
 
 const WithHeaderLayout = () => (
   <>
@@ -21,15 +21,13 @@ const NoHeaderLayout = () => <Outlet />;
 
 const AppRoutes = () => {
   return (
-    <>
-      <Routes>
-        {/* Header 포함 구간: Home, ProductDetail */}
-        <Route element={<WithHeaderLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/detail/:id" element={<ProductDetail />} />
-          <Route path="/user/:id" element={<UserPage />} />
-        </Route>
-
+    <Routes>
+      {/* Header 포함 구간: Home, ProductDetail */}
+      <Route element={<WithHeaderLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<ProductDetail />} />
+        <Route path="/user/:id" element={<UserPage />} />
+      </Route>
 
       {/* Header 미포함 구간: Signup, MyPage */}
       <Route element={<NoHeaderLayout />}>
@@ -40,7 +38,6 @@ const AppRoutes = () => {
       </Route>
       <Route path="/oauth/kakao/callback" element={<KakaoCallback />} />
     </Routes>
-
   );
 };
 
