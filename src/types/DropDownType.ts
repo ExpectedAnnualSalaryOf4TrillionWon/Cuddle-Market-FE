d515edc50b38cbf0f4079f54b4a08bb4c3244f59
dirage1 <dirage4635@gmail.com>
2025-08-20 19:29:08 +0900

    style: 기능 추가 중 넣었다가 뺀 타입단언  삭제

--- src/types/DropDownType.ts
+++ src/types/DropDownType.ts
@@ -3,5 +3,4 @@ export interface DropdownProps {
   // 드롭다운 활성화 여부는 boolean값을 통해 변경하게 된다.
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   //setIsOpen은 React.useState로부터 생성된 set함수로 boolean 값을 변경할 수 있는 타입이다.
-  alarms?: string[];
 }
