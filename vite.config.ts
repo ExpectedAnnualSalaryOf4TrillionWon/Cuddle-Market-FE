41028cd11c163a5a212cd0ae63ae0d524a9fdb10
jjub0217 <noonssub1004@gmail.com>
2025-11-17 14:59:52 +0900

    docs: 지금 당장 사용하지 않는 라이브러리 주석(#120)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
+// import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
