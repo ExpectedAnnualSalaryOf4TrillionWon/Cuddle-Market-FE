4f7e1406fb6d5b87a467b862cad7094b70682380
jjub0217 <noonssub1004@gmail.com>
2025-11-18 18:40:53 +0900

    feat: 사용자 섹션 컴포넌트 구현(#136)
    - HeaderUserSection: 로그인/비로그인 상태 분기 처리
    - AuthMenu: 비로그인 상태 인증 메뉴
    - UserMenu: 로그인 상태 사용자 드롭다운 메뉴
    - 프로필 정보 및 메뉴 항목 표시
    - CVA를 활용한 스타일 관리

---
+++ src/components/commons/header/components/user-section/AuthMenu.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom'
+import { ROUTES } from '@src/constants/routes'
+// import PageLink from '@components/commons/page-link/PageLink'
+const ACCOUNT = 'https://account.ozcoding.site'
+const returnTo = encodeURIComponent(window.location.href)
+export default function AuthMenu() {
+  return (
+    <>
+      <Link
+        to={ROUTES.LOGIN}
+        // variant="text"
+        // fontWeight="normal"
+        // link={`${ACCOUNT}/auth/login?return_to=${returnTo}`}
+        // size="lg"
+      >
+        로그인
+      </Link>
+      <Link
+        to={ROUTES.SIGNUP}
+        // variant="filled"
+        // size="base"
+        // fontWeight="medium"
+        // link="/signup"
+        className="rounded-lg bg-white px-4 py-2.5"
+      >
+        회원가입
+      </Link>
+    </>
+  )
+}
