e92e35e1958fbb0168a5036b086704a34a3bb975
jjub0217 <noonssub1004@gmail.com>
2025-12-17 23:36:06 +0900

    design: 푸터 모바일 UI 개선(#295)

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <footer className="border-border bg-light border-t">
       <div className="px-lg mx-auto max-w-7xl py-12">
-        <div className="flex justify-between">
+        <div className="flex flex-wrap justify-baseline gap-x-10 gap-y-5 md:justify-between md:gap-0">
           <div className="flex flex-col gap-2">
             <Logo logoClassname="h-12" textClassname="text-gray-400" />
             <p className="text-gray-500">반려동물과 함께하는 행복한 반려동물 용품 거래를 경험하세요.</p>
