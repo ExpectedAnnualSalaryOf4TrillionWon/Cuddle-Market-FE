d0d758ab0cce052ec07fafb4656b824c2fc1d94c
jjub0217 <noonssub1004@gmail.com>
2025-12-19 23:34:19 +0900

    refactor: 채팅 페이지 디렉토리명 케밥 케이스 적용(#313)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,7 +23,7 @@ import CommunityPage from '@src/pages/community/CommunityPage'
 import CommunityPostForm from '@src/pages/community/components/CommunityPostForm'
 import CommunityDetail from '@src/pages/community/CommunityDetail'
 import GoogleCallback from '@src/pages/GoogleCallback'
-import ChattingPage from '@src/pages/chattingpage/ChattingPage'
+import ChattingPage from '@src/pages/chatting-page/ChattingPage'
 
 const routes = [
   { path: ROUTES.HOME, element: <Home /> },
