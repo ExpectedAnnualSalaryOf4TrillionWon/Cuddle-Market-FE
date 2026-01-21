7867cf01ffc1a1ce3305a00f67f7b8aba3f7768a
jjub0217 <noonssub1004@gmail.com>
2026-01-21 17:21:27 +0900

    feat: 소셜 회원가입 페이지 안내 문구 추가(#542)

--- src/pages/signup/SocialSignup.tsx
+++ src/pages/signup/SocialSignup.tsx
@@ -1,9 +1,11 @@
+import { TitleSection } from '../login/components/TitleSection'
 import { SocialSignUpForm } from './components/SocialSignUpForm'
 
 export default function SocialSignup() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-baseline bg-white py-10 md:justify-center md:bg-[#F3F4F6]">
       <div className="flex min-w-full flex-col items-center gap-9 rounded-[20px] bg-white px-5 md:min-w-[530px] md:py-10">
+        <TitleSection title="회원가입" desc="추가정보를 입력해주세요" />
         <SocialSignUpForm />
       </div>
     </div>
